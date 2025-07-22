424a608c5b176e600f8d6024595cd0bdd97f74c3
Alvin Kong <aalvinkong03@gmail.com>
2025-07-21 17:40:07 -0700

    implementation file for the UI module of the Ferry Reservation System

---
+++ ui.cpp
@@ -0,0 +1,198 @@
+//================================================================
+//================================================================
+
+/*
+ * Filename: ui.cpp
+ * 
+ * Revision History: 
+ * Rev. 1 - 25/07/21 Original by A. Kong
+ *
+ * Description: UI of the Ferry Reservation System,
+ *              displays the menu to user, takes commands.
+ *              
+ */
+
+//================================================================
+
+#include <string>
+#include <iostream>
+#include "ui.hpp"
+
+// different submenus user can be in, start at main menu
+enum menu{mainMenu, sailingMenu, reservationMenu, exitProgram};
+enum menu currentMenu = mainMenu;
+
+void sailingManagement()
+{
+    currentMenu = sailingMenu;
+    return;
+}
+
+void reservation()
+{
+    currentMenu = reservationMenu;
+    return;
+}
+
+void createVessel()
+{
+    Vessel userVessel;
+    std::cout << "Please enter a valid vessel name (max 20 char.)" << std::endl;
+    cin >> userVessel.name;
+    std::cout << "Please enter the low ceiling lane length of the vessel" << std::endl;
+    cin >> userVessel.LCLL;
+    std::cout << "Please enter the high ceiling lane length of the vessel" << std::endl;
+    cin >> userVessel.HCLL;
+    writeVessel(userVessel);
+}
+
+// Function takes user input and takes an action
+// depending on which menu the user is in
+void processInput()
+{
+    // variable initialization
+    int userInput;
+    std::string printerName; // MAY NEED TO CHANGE THIS VAR TYPE
+    std::string sailingID; // MAY NEED TO CHANGE THIS VAR TYPE
+    std::string vehicleLicence; // MAY NEED TO CHANGE VAR TYPE
+    std::string vesselName; // MAY NEED TO CHANGE VAR TYPE
+    std::cout << "Enter choice: " << std::endl;
+    std::cin >> userInput;
+
+    switch(currentMenu)
+    {
+    case mainMenu:
+        switch (userInput)
+        {
+        // access reservation submenu
+        case 1:
+            reservation();
+            break;
+        // access sailing submenu
+        case 2:
+            sailingManagement();
+            break;
+        // create a new vessel
+        case 3:
+            createVessel();
+            break;
+        // exitProgram program
+        case 4:
+            currentMenu = exitProgram;
+        // invalid user input
+        default:
+            std::cout << "Please select a valid option" << std::endl;
+            break;
+        }
+    case reservationMenu:
+        switch (userInput)
+        {
+        // create a reservation
+        case 1:
+            std::cout << "Please enter a valid sailing ID" << std::endl;
+            std::cin >> sailingID;
+            std::cout << "Please enter the vehicle's licence plate" << std::endl;
+            std::cin >> vehicleLicence;
+            createReservation(sailingID, vehicleLicence);
+            break;
+        // delete a reservation
+        case 2:
+            std::cout << "Please enter a sailing ID" << std::endl;
+            std::cin >> sailingID;
+            std::cout << "Please enter the vehicle's licence plate" << std::endl;
+            std::cin >> vehicleLicence;
+            deleteReservations(sailingID, vehicleLicence);
+            break;
+        // return to main menu
+        case 3:
+            currentMenu = mainMenu;
+        // invalid user input
+        default:
+            std::cout << "Please select a valid option" << std::endl;
+            break;
+        }
+    case sailingMenu:
+        switch (userInput)
+        {
+        // customer check in 
+        case 1:
+            std::cout << "Please enter a valid sailing ID" << std::endl;
+            std::cin >> sailingID;
+            std::cout << "Please enter the vehicle's licence plate" << std::endl;
+            std::cin >> vehicleLicence;
+            checkInReservation(sailingID, vehicleLicence);
+            break;
+        // create sailing
+        case 2:
+            std::cout << "Please enter a valid vessel name" << std::endl;
+            std::cin >> vesselName;
+            createSailing(vesselName);
+            break;
+        // query sailing
+        case 3:
+            querySailing();
+        // delete sailing
+        case 4:
+            removeReservations(querySailing());
+        // print sailing report
+        case 5:
+            cout << "Please enter the name"
+                << " of the desired printing location." << std::endl;
+            cin >> printerName;
+            printSailingReport(printerName);
+        // return to main menu
+        case 6:
+            currentMenu = mainMenu;
+        // invalid user input
+        default:
+            std::cout << "Please select a valid option" << std::endl;
+            break;
+        }
+    case exitProgram:
+        break;
+    }
+    return;
+}
+
+// Function displays the appropriate menu to the user depending on currentMenu
+void displayCurrentMenu()
+{
+    std::cout << "Welcome to the Ferry Reservation System!" << std::endl;
+    // display menus and take input until user decides to exit
+    while (currentMenu != exitProgram)
+    {
+        switch(currentMenu)
+        {
+        case mainMenu:
+            std::cout << "\n=== Main Menu ===\n"
+                << "1. Reservation Submenu\n"
+                << "2. Sailing Submenu\n"
+                << "3. Create Vessel\n"
+                << "4. Exit" << std::endl;
+            processInput();
+        case reservationMenu:
+            std::cout << "\n=== Reservation Menu ===\n"
+                << "1. Create Reservation\n"
+                << "2. Delete Reservation\n"
+                << "3. Return to Main Menu" << std::endl;
+            processInput();
+        case sailingMenu:
+            std::cout << "\n=== Sailing Menu ===\n"
+                << "1. Check In\n"
+                << "2. Create Sailing\n"
+                << "3. Query Sailing\n"
+                << "4. Delete Sailing\n"
+                << "5. Print Sailing Report\n"
+                << "6. Return to Main Menu" << std::endl;
+            processInput();
+        }
+    }
+    std::cout << "Exiting Ferry Reservation System. Goodbye!" << std::endl;
+    return;
+}
+
+int main()
+{
+    displayCurrentMenu();
+    return 0;
+}
